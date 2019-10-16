830e5bc2942af1c85aade47e1bcd59182ac757e1
高辛格 <xg.gao@tianrang-inc.com>
2019-10-16 23:42:28 +0800

    visualization

--- visualization/visualization1.py
+++ visualization/visualization1.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras import Model
-from tensorflow.keras.applications import VGG16
 from cs231n.classifiers.squeezenet import SqueezeNet
 tf.enable_eager_execution()
 
@@ -17,7 +16,11 @@ def deprocess(image):
      return np.clip(image, 0, 255).astype(np.uint8)
 
 
-l2_reg = 1e-5
+def clip(image):
+    return tf.clip_by_value(image, clip_value_min=0, clip_value_max=1)
+
+
+l2_reg = 1e-3
 learning_rate = 25
 epochs = 5000
 epoch0 = 10
@@ -25,10 +28,6 @@ epoch1 = 1000
 target = 76
 MEAN = np.float32([0.485, 0.456, 0.406])
 STD = np.float32([0.229, 0.224, 0.225])
-
-"""
-model = VGG16(include_top=True, weights="imagenet")
-"""
 
 SAVE_PATH = "cs231n/datasets/squeezenet.ckpt"
 if not os.path.exists(SAVE_PATH + ".index"):
@@ -47,6 +46,7 @@ for _ in range(epochs):
         loss = model(X)[0, target] - l2_reg * tf.nn.l2_loss(X)
     dX = tape.gradient(loss, X)
     X.assign_add(dX[0] * learning_rate)
+    X.assign(clip(X))
     
     if _ % epoch0 == 0:
         print(_, loss.numpy())
