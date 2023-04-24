c7de59c75c0ca16e0b33bd4f1d2a1c21f6e00ffe
gaoxinge <gaoxx5@gmail.com>
2023-04-24 10:08:15 +0800

    tensorflow

--- tensorflow/other/tfgfile/1/README.md
+++ tensorflow/other/tfgfile/1/README.md
@@ -1,3 +1,5 @@
+## reference
+
 - [TensorFlow学习笔记：tf.gfile文件读写](https://www.jianshu.com/p/d8f5357b95b3)
 - [Why use tensorflow gfile? (for file I/O)](https://stackoverflow.com/questions/42922948/why-use-tensorflow-gfile-for-file-i-o/)
 - [tensorflow gfile文件操作详解](https://zhuanlan.zhihu.com/p/31536538)
--- tensorflow/other/tfgfile/1/a.py
+++ tensorflow/other/tfgfile/1/a.py
@@ -1,25 +1,25 @@
 import tensorflow as tf
 
 # test read
-with tf.gfile.GFile("20000.jpg", "rb") as fid:
+with tf.gfile.GFile("assets/20000.jpg", "rb") as fid:
     a = fid.read()
 
-with tf.gfile.FastGFile("20000.jpg", "rb") as fid:
+with tf.gfile.FastGFile("assets/20000.jpg", "rb") as fid:
     b = fid.read()
 
-with open("20000.jpg", "rb") as fid:
+with open("assets/20000.jpg", "rb") as fid:
     c = fid.read()
 
 print(a == b == c)
 
 # test readlines
-with tf.gfile.GFile("tmp.txt") as fid:
+with tf.gfile.GFile("assets/tmp.txt") as fid:
     a = fid.readlines()
 
-with tf.gfile.FastGFile("tmp.txt") as fid:
+with tf.gfile.FastGFile("assets/tmp.txt") as fid:
     b = fid.readlines()
 
-with open("tmp.txt") as fid:
+with open("assets/tmp.txt") as fid:
     c = fid.readlines()
 
 print(a == b == c)
