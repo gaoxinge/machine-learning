93699a42285924d4df7a910bb5978c6c5442811d
gaoxinge <gaoxx5@gmail.com>
2023-06-09 01:31:00 +0800

    cs231n

--- cs231n/assignment/assignment3/network_visualization_activation_4.py
+++ cs231n/assignment/assignment3/network_visualization_activation_4.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.keras.backend import get_session
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import ZeroPadding2D, Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.optimizers import Adam
@@ -10,9 +9,8 @@ from tensorflow.keras.metrics import sparse_categorical_accuracy
 
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-x_train = x_train.reshape(x_train.shape[0], 28, 28, 1).astype(np.float32) / 255
-x_test = x_test.reshape(x_test.shape[0], 28, 28, 1).astype(np.float32) / 255
-
+x_train = x_train.astype(np.float32) / 255
+x_test = x_test.astype(np.float32) / 255
 
 model = Sequential()
 model.add(ZeroPadding2D(2, input_shape=(28, 28, 1)))
@@ -26,12 +24,12 @@ model.add(Dense(units=84, activation=tf.nn.relu))
 model.add(Dense(units=10, activation=tf.nn.softmax))
 model.compile(optimizer=Adam(0.001), loss=sparse_categorical_crossentropy, metrics=[sparse_categorical_accuracy])
 model.fit(x=x_train, y=y_train, epochs=10)
-loss, acc = model.evaluate(x=x_test, y=y_test)
-print("loss: %s, acc: %s" % (loss, acc))
+loss, acc = model.evaluate(x=x_test, y=y_test, verbose=0)
+print(f"loss: {loss}, acc: {acc}")
 
 
 def process(image):
-    return X.astype(np.float32) / 255
+    return image.astype(np.float32) / 255
 
 
 def deprocess(image):
@@ -50,25 +48,22 @@ epoch0 = 10
 epoch1 = 10
 target = 9
 
-sess = get_session()
 X = 255 * np.random.rand(28, 28, 1)
-X = process(X)
-X = X[None]
+X = process(X)[None]
+X = tf.Variable(X)
 for _ in range(epochs):
-    Y = tf.convert_to_tensor(X)
     with tf.GradientTape() as tape:
-        tape.watch(Y)
-        loss = model(Y)[0, target] - l2_reg * tf.nn.l2_loss(Y)
-    dY = tape.gradient(loss, Y)
-    loss, dX = sess.run([loss, dY])
-    X += dX[0] * learning_rate
-    X = clip(X)
+        tape.watch(X)
+        loss = model(X)[0, target] - l2_reg * tf.nn.l2_loss(X)
+    dX = tape.gradient(loss, X)
+    X.assign_add(dX * learning_rate)
+    X.assign(clip(X))
 
     if _ % epoch0 == 0:
-        print(_, loss)
+        print(_, loss.numpy())
 
     if _ % epoch1 == 0:
-        Y = deprocess(X[0])
-        plt.imshow(Y[:, :, 0], cmap='gray', vmin=0, vmax=255)
+        plt.imshow(deprocess(X[0]), cmap="gray", vmin=0, vmax=255)
         plt.axis("off")
         plt.savefig("image/%s.jpg" % _)
+
