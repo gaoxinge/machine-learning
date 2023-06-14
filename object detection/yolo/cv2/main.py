da3d58b81f0a81303b589686414e0a5c7f3ca5c0
gaoxinge <gaoxx5@gmail.com>
2023-06-14 10:14:20 +0800

    object detection

--- object detection/yolo/cv2/main.py
+++ object detection/yolo/cv2/main.py
@@ -2,13 +2,13 @@ import cv2
 
 net = cv2.dnn.readNetFromDarknet("yolov3.cfg", "yolov3.weights")
 model = cv2.dnn_DetectionModel(net)
-model.setInputParams(scale=1 / 255, size=(416, 416), swapRB=True)
+model.setInputParams(scale=1/255, size=(416, 416), swapRB=True)
 
 img = cv2.imread("dog.jpg")
 class_ids, scores, boxes = model.detect(img, 0.5, 0.4)
 for (class_id, score, box) in zip(class_ids, scores, boxes):
-    cv2.rectangle(img, box, (0, 0, 0), 2)
-    cv2.putText(img, str(class_id), (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
+    cv2.rectangle(img, box, (0, 0, 0), 1)
+    cv2.putText(img, str(class_id), (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1)
 
 cv2.imshow("", img)
 cv2.waitKey(0)
